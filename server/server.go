9e0fe6827d3c2e1ae4894fdd099baff59a5408b6
Alec Waichunas <awaichunas@gmail.com>
2019-02-27 17:53:20 -0600

    Added basic server code

---
+++ server/server.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "net"
+
+func main(){
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error creating server")
+		return;
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error connecting to client")
+		}else{
+			fmt.Println("Connected to client")
+			conn.Close()
+		}
+	}
+}
